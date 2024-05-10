19815df96631d149b676d865b66fe787ac4fa9f8
trippyVaibhav <145649573+trippyVaibhav@users.noreply.github.com>
2024-05-10 15:07:33 +0530

    first commit

---
+++ awtConfigurations.js
@@ -0,0 +1,34 @@
+// Fixed Aspect Ratio = 0
+// Max Aspect Ratio = 1
+// Min Aspect Ratio = 2
+// Max-Min Aspect Ratio (MMAR) = 3
+// Maximize To Window = 4
+var options = 4;
+
+
+
+// if it's not necessary, enter "1"
+
+// Max Aspect Ratio
+var maxAspectRatioWidth = 1;
+var maxAspectRatioHeight = 1;
+
+// Min Aspect Ratio
+var minAspectRatioWidth = 1;
+var minAspectRatioHeight = 1;
+
+// Fixed Aspect Ratio
+var fixedAspectRatioWidth = 20;
+var fixedAspectRatioHeight = 9;
+
+/////////////////////////////////////////////////////////////////////////
+
+var startToLoadButton = false;
+
+/////////////////////////////////////////////////////////////////////////
+
+var watchVideoButtonAutoSize = false;
+var playPongButtonAutoSize = false;
+
+var watchVideoButtonFontSize = "12px";
+var playPongButtonFontSize = "13px";
